eb28c5e8a1ac0e85f907e77d31b480563c4e4ae9
Rafael S. de Lima <rafael.lima@startse.com>
2022-08-01 06:07:14 -0300

    Criação dos arquivos Vetor, LDE e AR, porém, sem implementação

---
+++ src/tp3EstruturaDados/app/estruturaDeDados/Vetor.java
@@ -0,0 +1,50 @@
+package tp3EstruturaDados.app.estruturaDeDados;
+
+import tp3EstruturaDados.app.entities.Veiculo;
+import tp3EstruturaDados.app.entities.interfaces.IMap;
+
+public class Vetor implements IMap{
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean containsKey(Object key) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean containsValue(Object value) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Veiculo get(Object key) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Veiculo put(int key, Veiculo value) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Veiculo remove(Object key) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
