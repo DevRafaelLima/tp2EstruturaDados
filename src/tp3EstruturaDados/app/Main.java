f3a0aa5df82b66b6bb38903a9be7a6ea0b3b79b3
Rafael S. de Lima <rafael.lima@startse.com>
2022-08-02 03:33:34 -0300

    implementação do tad

--- src/tp3EstruturaDados/app/Main.java
+++ src/tp3EstruturaDados/app/Main.java
@@ -1,21 +1,36 @@
 package tp3EstruturaDados.app;
 
 import tp3EstruturaDados.app.entities.Veiculo;
-import tp3EstruturaDados.app.estruturaDeDados.TAD;
+import tp3EstruturaDados.app.estruturaDeDados.MapaHashVetor;
 
 public class Main {
 	private static void popula() {
-		//TAD
-		TAD tad = new TAD();
-		
+//		//TAD
+//		TAD tad = new TAD();
+//		
+//		int aux = 0;
+//		while(aux < 100000) {
+//			//veiculo
+//			Veiculo veiculo = new Veiculo();
+//			tad.add(veiculo);
+//			aux++;
+//		}
+//		tad.impressao();
+		MapaHashVetor mhv = new MapaHashVetor();
 		int aux = 0;
-		while(aux < 100000) {
-			//veiculo
+		while (aux < 10) {
 			Veiculo veiculo = new Veiculo();
-			tad.add(veiculo);
+			mhv.put(veiculo.getChassi(), veiculo);	
+			System.out.println(mhv.get(veiculo.getChassi()));
 			aux++;
 		}
-		tad.impressao();
+		System.out.println("Ordena chassi");
+		mhv.ordenaChassi();
+		System.out.println("Impressão");
+		mhv.impressao();
+//		System.out.println("Remove chassis");
+//		mhv.removeChassis();
+		System.out.println("Finalizou");
 	}
 	public static void main(String[] args) {
 		popula();
